dec41f7ba1ab9eae8050f281e9c306651dd278a2
nizmogr <64306122+nizmogr@users.noreply.github.com>
2022-04-02 00:11:29 +0300

    Delete server directory

--- server/server.lua
+++
@@ -1,9 +0,0 @@
--- Don't edit this file
-local neededGameBuild = 2060
-local currentGameBuild = GetConvarInt('sv_enforceGameBuild', 1604)
-
-Citizen.CreateThread(function()
-    if (currentGameBuild < neededGameBuild) then
-        print('^3['..GetCurrentResourceName()..']^0: You need to use ^3' .. neededGameBuild .. '^0 game build (or above) to use this resource.')
-    end
-end)
